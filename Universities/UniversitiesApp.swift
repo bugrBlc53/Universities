502238483f0ce28317c7492ccdbaa383e1a22b45
bugrBlc53 <147655449+bugrBlc53@users.noreply.github.com>
2023-10-11 22:13:01 +0300

    Initial commit

---
+++ Universities/UniversitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversitiesApp.swift
+//  Universities
+//
+//  Created by Mehmet Buğra BALCI on 11.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct UniversitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
